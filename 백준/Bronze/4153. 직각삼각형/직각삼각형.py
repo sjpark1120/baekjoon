1dd65177aaddb83e45494c491cdc68d459b1215d
Sujin Park <69676617+sjpark1120@users.noreply.github.com>
2023-08-31 02:37:56 +0900

    [Bronze III] Title: 직각삼각형, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,14 @@
+while(True):
+    arr = list(map(int, input().split()))
+    if arr[0] ==0 and arr[1] == 0 and arr[2] == 0:
+        break
+    bigindex = 0
+
+    if arr[0] ** 2 + arr[1] ** 2 == arr[2] ** 2:
+        print("right")
+    elif arr[0] ** 2 + arr[2] ** 2 == arr[1] ** 2:
+        print("right")
+    elif arr[1] ** 2 + arr[2] ** 2 == arr[0] ** 2:
+        print("right")
+    else:
+        print("wrong")
