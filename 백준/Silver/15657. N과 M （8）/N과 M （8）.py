32ff67266df2c0c8f4f62c60c541ae176eb2b6e8
Sujin Park <69676617+sjpark1120@users.noreply.github.com>
2023-08-31 16:22:33 +0900

    [Silver III] Title: N과 M (8), Time: 104 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,21 @@
+import sys
+n, m = map(int, sys.stdin.readline().split())
+input = [0] * 10
+input = list(map(int, sys.stdin.readline().split()))
+input.sort()
+arr = [0] * 10
+used = [False] * 10
+def nANDm(k):
+    if k == m:
+        for i in range(m):
+            print(arr[i], end = ' ')
+        print("")
+        return 0
+    else:
+        for i in range(1, n+1):
+            if k==0 or arr[k-1] <= input[i-1]:
+                arr[k] = input[i-1]
+                used[i] = True
+                nANDm(k+1)
+                used[i] = False
+nANDm(0)
