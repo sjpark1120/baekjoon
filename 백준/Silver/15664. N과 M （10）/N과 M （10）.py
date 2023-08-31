73e2807f1c4169c2f309302e2327fc0069343446
Sujin Park <69676617+sjpark1120@users.noreply.github.com>
2023-08-31 16:23:43 +0900

    [Silver II] Title: N과 M (10), Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,24 @@
+import sys
+n, m = map(int, sys.stdin.readline().split())
+input = list(map(int, sys.stdin.readline().split()))
+input.sort()
+arr = []
+used = [False] * 10
+
+def nANDm(k):
+    if k == m:
+        for i in range(m):
+            print(arr[i], end = ' ')
+        print("")
+        return 0
+    else:
+        overlap = 0
+        for i in range(n):
+            if not used[i] and overlap != input[i]and (k==0 or arr[k-1]<=input[i]):
+                arr.append(input[i])
+                overlap = input[i]
+                used[i] = True
+                nANDm(k+1)
+                used[i] = False
+                arr.pop()
+nANDm(0)
