4221869427d2382af345120cba55518830debd6c
Sujin Park <69676617+sjpark1120@users.noreply.github.com>
2024-06-28 19:14:13 +0900

    [Silver III] Title: N과 M (4), Time: 68 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,18 +1,17 @@
 import sys
+
+def nandm(k):
+  if k == m:
+    for i in range(m):
+      print(arr[i], end = ' ')
+    print('')
+    return 0 
+  for i in range(1, n+1):
+    if k == 0 or arr[k-1] <= i:
+      arr[k] = i
+      nandm(k+1)
+
 n, m = map(int, sys.stdin.readline().split())
 arr = [0] * 10
-used = [False] * 10
-def nANDm(k):
-    if k == m:
-        for i in range(m):
-            print(arr[i], end = ' ')
-        print("")
-        return 0
-    else:
-        for i in range(1, n+1):
-            if k==0 or arr[k-1]<i+1:
-                arr[k] = i
-                used[i] = True
-                nANDm(k+1)
-                used[i] = False
-nANDm(0)
+isused = [False] * 10
+nandm(0)
