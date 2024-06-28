a2f034e39611c5424d71f928f329b62c2b029505
Sujin Park <69676617+sjpark1120@users.noreply.github.com>
2024-06-29 01:03:41 +0900

    [Silver III] Title: N과 M (7), Time: 2864 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.py
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -1,21 +1,17 @@
 import sys
-n, m = map(int, sys.stdin.readline().split())
-input = [0] * 10
-input = list(map(int, sys.stdin.readline().split()))
-input.sort()
-arr = [0] * 10
-used = [False] * 10
-def nANDm(k):
-    if k == m:
-        for i in range(m):
-            print(arr[i], end = ' ')
-        print("")
-        return 0
-    else:
-        for i in range(1, n+1):
 
-                arr[k] = input[i-1]
-                used[i] = True
-                nANDm(k+1)
-                used[i] = False
-nANDm(0)
+def nandm(k):
+  if k == m:
+    for i in range(m):
+      print(arr[i], end = ' ')
+    print('')
+    return 0 
+  for i in range(n):
+      arr[k] = n_list[i]
+      nandm(k+1)
+
+n, m = map(int, sys.stdin.readline().split())
+n_list = list(map(int, sys.stdin.readline().split()))
+n_list.sort()
+arr = [0] * 10
+nandm(0)
