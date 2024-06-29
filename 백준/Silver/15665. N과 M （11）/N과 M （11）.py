62ff92104fe352dd1b0995d1c96bdbd6f46e0d4d
Sujin Park <69676617+sjpark1120@users.noreply.github.com>
2024-06-29 15:03:29 +0900

    [Silver II] Title: N과 M (11), Time: 940 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.py
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -1,24 +1,23 @@
 import sys
-n, m = map(int, sys.stdin.readline().split())
-input = list(map(int, sys.stdin.readline().split()))
-input.sort()
-arr = []
-used = [False] * 10
 
-def nANDm(k):
-    if k == m:
-        for i in range(m):
-            print(arr[i], end = ' ')
-        print("")
-        return 0
-    else:
-        overlap = 0
-        for i in range(n):
-            if overlap != input[i]:
-                arr.append(input[i])
-                overlap = input[i]
-                used[i] = True
-                nANDm(k+1)
-                used[i] = False
-                arr.pop()
-nANDm(0)
+def nandm(k):
+  if k == m:
+    for i in range(m):
+      print(arr[i], end = ' ')
+    print('')
+    return 0 
+  check = 0
+  for i in range(n):
+    if check != n_list[i]:
+      arr[k] = n_list[i]
+      check = n_list[i]
+      isused[i] =True
+      nandm(k+1)
+      isused[i] = False
+
+n, m = map(int, sys.stdin.readline().split())
+n_list = list(map(int, sys.stdin.readline().split()))
+n_list.sort()
+arr = [0] * 10
+isused = [False] * 10
+nandm(0)
