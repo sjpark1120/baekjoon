553df041ae1748da01ff8553878a6e1e45270227
Sujin Park <69676617+sjpark1120@users.noreply.github.com>
2024-06-29 13:49:21 +0900

    [Silver II] Title: N과 M (9), Time: 108 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,23 @@
+import sys
+
+def nandm(k):
+  if k == m:
+    for i in range(m):
+      print(arr[i], end = ' ')
+    print('')
+    return 0 
+  check = 0
+  for i in range(n):
+    if not isused[i] and check != n_list[i]:
+      arr[k] = n_list[i]
+      check = n_list[i]
+      isused[i] =True
+      nandm(k+1)
+      isused[i] = False
+
+n, m = map(int, sys.stdin.readline().split())
+n_list = list(map(int, sys.stdin.readline().split()))
+n_list.sort()
+arr = [0] * 10
+isused = [False] * 10
+nandm(0)
