d991e65ffda6eb93bc84f9d5004d61bc3ce6dc14
Sujin Park <69676617+sjpark1120@users.noreply.github.com>
2026-05-20 09:46:56 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.01 ms, Memory: 9.03 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -0,0 +1,18 @@
+def solution(keyinput, board):
+    x, y = 0,0
+    max_x = board[0]//2
+    max_y = board[1]//2
+    for key in keyinput:
+        if key == "up":
+            if y < max_y:
+                y += 1
+        elif key == "down":
+            if y > -max_y:
+                y -= 1
+        elif key == "left":
+            if x > -max_x:
+                x -= 1
+        elif key == "right":
+            if x < max_x:
+                x += 1
+    return [x,y]
